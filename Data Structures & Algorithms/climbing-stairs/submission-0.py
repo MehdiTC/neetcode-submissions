f9c66cfa328455d24356388f1448ae3978d57804
Mehdi Touhami Chahdi <mehdi.touhamic@gmail.com>
2026-07-20 16:33:10 +0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n==0: return 0
+        if n==1: return 1
+
+        prev = 0
+        curr = 1
+
+        for _ in range(1, n+1):
+            prev, curr = curr, prev+curr
+        
+        return curr
+        
